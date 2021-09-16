ede3637021214cf798894e95d5e2dc13d62c79ce
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2021-09-15 22:20:41 -0300

    imagem do card

--- src/componentes/Card/CardOne/index.js
+++ src/componentes/Card/CardOne/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import autocad from '../autocad.png';
 import './style.css';
 
 function CardOne() {
     return(
         <div className="card">
-            <div style={{ backgroundImage: `src(${autocad})` }}>
-                HelloWorld
-            </div>
+            <div className="img-card"></div>
+            
             <h3>Auto Cad 2D</h3>
             <p>Duração: 48 Horas</p>
             <p>Valor: </p>
