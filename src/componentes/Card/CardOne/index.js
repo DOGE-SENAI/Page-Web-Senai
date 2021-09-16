afa5b855e03bae41acf2db80ed908b3ad49d0a9f
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2021-09-15 23:19:08 -0300

    infos card

--- src/componentes/Card/CardOne/index.js
+++ src/componentes/Card/CardOne/index.js
@@ -5,10 +5,12 @@ function CardOne() {
     return(
         <div className="card">
             <div className="img-card"></div>
-            
-            <h3>Auto Cad 2D</h3>
-            <p>Duração: 48 Horas</p>
-            <p>Valor: </p>
+            <div className="text-card">
+                <h3>Auto Cad 2D</h3>
+                <p><span className="text-bold">Carga Horária:</span> 48 Horas</p>
+                <p><span className="text-bold">Área:</span> Tecnologia da Informação</p>
+                <p><span className="text-bold">Valor:</span> R$59,99</p>
+            </div>
         </div>
     )
 }
