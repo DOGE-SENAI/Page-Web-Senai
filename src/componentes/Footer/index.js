0e0a826d8d9636843a36e28bbf47c454e766b746
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2021-09-16 13:15:56 -0300

    Footer adicionado

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return(
+        <footer>
+            <p>DOGE © 2021</p>
+        </footer>
+    );
+}
+
+export default Footer;
